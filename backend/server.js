e212ff9ef6347402c121b72fad45277ea4e8acd6
Ruslan <dolgopol.r.v@gmail.com>
2021-07-10 15:00:20 +0300

    created registerUser handler on backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -14,8 +15,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
-
+app.use(express.json())
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
